39d1bc149f144223d8ec43de1157ca0ec42ed92e
Chloe <cmontroy99@gmail.com>
2024-05-07 22:23:33 -0500

    arrays are fun so far!

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,14 @@
 //Create an array called practiceFile with the following entry: 273.15
-
+let practiceFile = [273.15];
+console.log(practiceFile);
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
+let valueOne = "42";
+let valueTwo = "hello";
+practiceFile.push(valueOne);
+practiceFile.push(valueTwo);
+console.log(practiceFile);
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+let valueThree = -4.6;
+let valueFour = "87"
+practiceFile.push(valueThree, valueFour);
+console.log(practiceFile);
