cad563347e34dc91519335abd2037a966303d130
Chloe <cmontroy99@gmail.com>
2024-06-06 17:25:56 -0500

    inchresting

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,17 +2,53 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
-};
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
+   };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
 
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let creatureCrew = [superChimpOne, superChimpTwo, salamander, dog, tardigrade];
+
+function crewReports(creatureCrew) {
+   for (let i = 0; i < 11; i++) {  
+   }  return creatureCrew;   
+} console.log(`${creatureCrew.name} is a ${creatureCrew.species}. They are ${creatureCrew.age} years old and ${creatureCrew.mass} kilograms. Their ID is ${creatureCrew.astronautID}.`)
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Add a move method to each animal object
